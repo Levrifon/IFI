0f320af1cdfeeb9bdfec77a5ed21b8afb29096f1
Levrifon <rems-msn@hotmail.fr>
2016-10-03 11:35:10 +0200

    debut de l'exemple avec MongoDB

--- src/main/java/tiir_ifi/project/Application.java
+++ src/main/java/tiir_ifi/project/Application.java
@@ -1,7 +1,14 @@
 package tiir_ifi.project;
 
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;;
 
 /**
  * Hello world!
@@ -11,5 +18,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		try {
+			MongoClient mongo = new MongoClient( "localhost" , 27017 );
+			DB db = mongo.getDB("chatdb");
+			DBCollection table = db.getCollection("log");
+			BasicDBObject document = new BasicDBObject();
+			document.put("pseudo","azuro");
+			document.put("message", "salut");
+			table.insert(document);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
