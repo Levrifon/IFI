6ec370e5b198ef4372e8c507c44b9eb71793814c
Levrifon <rems-msn@hotmail.fr>
2016-10-03 11:36:47 +0200

    fixed ok

---
+++ src/main/java/tiir_ifi/project/HelloMessage.java
@@ -0,0 +1,18 @@
+package tiir_ifi.project;
+
+public class HelloMessage {
+	private String name;
+	public HelloMessage() {}
+	
+	public HelloMessage(String n) {
+		this.name = n;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+
+}
